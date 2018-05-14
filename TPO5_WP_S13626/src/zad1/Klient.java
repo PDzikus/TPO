bcbf3115e7c444712b06e6c147fd20d8fc0ace43
PDzikus <pawelwiszniewski@gmail.com>
2018-05-14 19:03:09 +0200

    TPO5 after klient MVC redesign

--- TPO5_WP_S13626/src/zad1/Klient.java
+++ TPO5_WP_S13626/src/zad1/Klient.java
@@ -19,49 +19,68 @@ import javax.swing.text.DefaultCaret;
 
 
 public class Klient extends JFrame{
-	/**
-	 * 
-	 */
+
 	private static final long serialVersionUID = 1L;
-	private SocketChannel channelToServer;
-	private SocketChannel channelFromServer;
+	private SocketChannel requestsChannel;
+	private SocketChannel messagesChannel;
 	private Selector selector;
 	private InetAddress serverIP;
 	private int serverPort;
 	private int id;
-    private HashSet<String> subscribedTopics = new HashSet<String>();
-    private HashSet<String> availableTopics = new HashSet<String>();
-    private boolean clientIsRunning = true;
+	private boolean clientIsRunning = true;
+	private Charset charset  = Charset.forName("UTF-8");
 	
-	public Klient(InetAddress IP, int port) {
+    private LinkedList<String> subscribedTopics;
+    private LinkedList<String> availableTopics;
+    private KlientGUI gui;
+	
+	public Klient(InetAddress IP, int port, KlientGUI gui) {
 		serverIP = IP;
 		serverPort = port;
-		Random rand = new Random();
-		id = rand.nextInt();
+		subscribedTopics = new LinkedList<>();
+		availableTopics = new LinkedList<>();
+		this.gui = gui;
+		
         try {
         	selector = Selector.open();
-        	channelFromServer = SocketChannel.open(new InetSocketAddress(serverIP, serverPort));
-            channelFromServer.configureBlocking(false);
-            channelFromServer.register(selector, channelFromServer.validOps());
-			channelToServer = SocketChannel.open(new InetSocketAddress(serverIP, serverPort));
-			channelToServer.configureBlocking(false);
-			//channelToServer.register(selector, channelToServer.validOps());
-            writeMessage("REGISTER::"+id, channelFromServer);
-            if (readMessage(channelFromServer).equals("OK"))
-            	log("połączenie do serwera otwarte");
+        	messagesChannel = SocketChannel.open(new InetSocketAddress(serverIP, serverPort));
+            messagesChannel.configureBlocking(false);
+            messagesChannel.register(selector, messagesChannel.validOps());
+			requestsChannel = SocketChannel.open(new InetSocketAddress(serverIP, serverPort));
+			requestsChannel.configureBlocking(false);
+            writeMessage("REGISTER", messagesChannel);
+            id = Integer.parseInt(readMessage(messagesChannel));
+           	log("połączenie do serwera otwarte");
         } catch (IOException e) {
             log("Nie mogę połączyć się z serwerem");
             System.exit(1);
         }
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-               	createAndShowGUI();
-            }
-        });
-		serviceConnection();
 	}
 	
-	private void serviceConnection() {
+	private void prepareGUI() {
+		gui.unsubTopicButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				unsubscribeTopic();
+			}
+		});	
+		gui.subscribeTopicButton.addActionListener(new ActionListener() {
+			public void actionPerformed (ActionEvent e) {
+				subscribeTopic();
+			}
+		});
+		gui.closeClientButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				//shutdownClient();
+			}
+		});
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+               	gui.start();
+            }
+        });
+	}
+	
+	public void start() {
 		while(clientIsRunning) {
 			try {
 				int readyCount = selector.select();
@@ -82,19 +101,18 @@ public class Klient extends JFrame{
 			} catch(Exception exc) {
 				log("straciłem połączenie z serwerem");
 				clientIsRunning = false;
-				displayMessage("informacja od serwera", "Straciłem połączenie z serwerem");
+				gui.displayMessage("informacja od serwera", "Straciłem połączenie z serwerem");
 			}
 		}
 		log("zamykam nasłuch");
 	}
 	
 	private void serviceRequest() {
-		SocketChannel channel = channelFromServer;
+		SocketChannel channel = messagesChannel;
 		String request = readMessage(channel);
 		String[] command = request.split("::");
-
 		if (command[0].equals("TOPIC_MESSAGE")) {
-			displayMessage(command[1], command[2]);
+			gui.displayMessage(command[1], command[2]);
 			writeMessage("OK", channel);          
 		}
 		else if (command[0].equals("TOPIC_CHANGE")) {
@@ -102,12 +120,10 @@ public class Klient extends JFrame{
 			refreshTopics();
 		}
 		else if (command[0].equals("SHUTDOWN")) {
-			displayMessage("informacja od serwera", "Serwer zakończył działanie");
+			gui.displayMessage("informacja od serwera", "Serwer zakończył działanie");
 		}
 	}
-
-	private Charset charset  = Charset.forName("UTF-8");
-		
+	
 	private String readMessage(SocketChannel channel) {
 		ByteBuffer buffer = ByteBuffer.allocate(1024);
 		if (!channel.isOpen()) return "";
@@ -155,219 +171,61 @@ public class Klient extends JFrame{
 	
 	private String sendRequest(String message) {
 		String response = "";
-		writeMessage(message, channelToServer);
-		response = readMessage(channelToServer);
+		writeMessage(message, requestsChannel);
+		response = readMessage(requestsChannel);
         return response;
 	}
 	
-	private void displayMessage(String topic, String message) {
-		String toDisplay = "[ "+LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss"))+" ] kanał: " + topic;
-		messageArea.append(toDisplay + "\r\n");
-		messageArea.append(message + "\r\n");
-		messageArea.append("\r\n");
-	}
-	
-	public static void log(String message){
+	private static void log(String message){
 		LocalTime time = LocalTime.now();
 		System.err.println("[" + time.toString() + "] Klient: "+ message);
 	}
 	
-	// zmienne dla GUI
-	private JTextArea messageArea;
-	private JList<String> subscribedTopicList;
-	private JList<String> availableTopicList;
-	
-	private void createAndShowGUI() {
-		setTitle("Klient");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(600,480);
-		
-		setLayout(new GridBagLayout());
-		GridBagConstraints position;
-		
-		position = new GridBagConstraints();
-		position.gridx = 0;
-		position.gridy = 0;
-		position.gridwidth = 4;
-		position.anchor = GridBagConstraints.WEST;
-		add(new JLabel("Zarządzanie kanałami:"), position);
-		
-		position = new GridBagConstraints();
-		position.gridx = 0;
-		position.gridy = 1;
-		add(Box.createRigidArea(new Dimension(10,5)), position);	
-		
-		position = new GridBagConstraints();
-		position.gridx = 0;
-		position.gridy = 2;
-		position.anchor = GridBagConstraints.WEST;
-		add(new JLabel("Kanały dostępne:"), position);
-		
-		position = new GridBagConstraints();
-		position.gridx = 4;
-		position.gridy = 2;
-		position.anchor = GridBagConstraints.WEST;
-		add(new JLabel("Kanały subskrybowane:"), position);
-		
-		availableTopicList = new JList<String>(new DefaultListModel<String>());
-		availableTopicList.setLayoutOrientation(JList.VERTICAL);
-		JScrollPane availablePane = new JScrollPane(availableTopicList);
-		position = new GridBagConstraints();
-		position.gridx = 0;
-		position.gridy = 3;
-		position.gridheight = 3;
-		position.anchor = GridBagConstraints.WEST;
-		availablePane.setPreferredSize(new Dimension (225,100));
-		add(availablePane,position);
-		
-		subscribedTopicList = new JList<String>(new DefaultListModel<String>());
-		subscribedTopicList.setLayoutOrientation(JList.VERTICAL);
-		JScrollPane subscribedPane = new JScrollPane(subscribedTopicList);
-		position = new GridBagConstraints();
-		position.gridx = 4;
-		position.gridy = 3;
-		position.gridheight = 3;
-		position.anchor = GridBagConstraints.EAST;
-		subscribedPane.setPreferredSize(new Dimension (225,100));
-		add(subscribedPane,position);
-		
-		refreshTopics();
-		
-		position = new GridBagConstraints();
-		position.gridx = 2;
-		position.gridy = 3;
-		add(Box.createRigidArea(new Dimension(10,15)), position);	
-		
-		position = new GridBagConstraints();
-		position.gridx = 1;
-		position.gridy = 4;
-		add(Box.createRigidArea(new Dimension(10,5)), position);	
-		
-		JButton removeTopicButton = new JButton("Zapisz >>");
-		removeTopicButton.addActionListener(new ActionListener() {
-			public void actionPerformed (ActionEvent e) {
-				subscribeTopic();
-			}
-		});
-		position = new GridBagConstraints();
-		position.gridx = 2;
-		position.gridy = 4;
-		position.anchor = GridBagConstraints.CENTER;
-		add(removeTopicButton, position);
-		
-		JButton refreshTopicListButton = new JButton("<< Usuń");
-		refreshTopicListButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				unsubscribeTopic();
-			}
-		});
-		position = new GridBagConstraints();
-		position.gridx = 2;
-		position.gridy = 5;
-		position.anchor = GridBagConstraints.CENTER;
-		add(refreshTopicListButton, position);
-		
-		position = new GridBagConstraints();
-		position.gridx = 3;
-		position.gridy = 4;
-		add(Box.createRigidArea(new Dimension(10,5)), position);	
-		
-		position = new GridBagConstraints();
-		position.gridx = 0;
-		position.gridy = 6;
-		add(Box.createRigidArea(new Dimension(10,20)), position);	
-		
-		position = new GridBagConstraints();
-		position.gridx = 0;
-		position.gridy = 7;
-		position.gridwidth = 3;
-		position.anchor = GridBagConstraints.WEST;
-		add(new JLabel("Wiadomość z wybranych kanałów:"), position);
-		
-		messageArea = new JTextArea(10,50);
-		messageArea.setLineWrap(true);
-		DefaultCaret caret = (DefaultCaret)messageArea.getCaret();
-		caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
-		JScrollPane messagePane = new JScrollPane(messageArea);
-		messagePane.setViewportView(messageArea);
-		position = new GridBagConstraints();
-		position.gridx = 0;
-		position.gridy = 8;
-		position.gridwidth = 5;
-		add(messagePane, position);
-		
-		position = new GridBagConstraints();
-		position.gridx = 0;
-		position.gridy = 9;
-		add(Box.createRigidArea(new Dimension(10,30)), position);	
-		
-		JButton sendMessageButton = new JButton("Zamknij połączenie");
-		sendMessageButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent event) {
-				//messageForTopics();
-			}
-		});
-		position = new GridBagConstraints();
-		position.gridx = 4;
-		position.gridy = 10;
-		position.gridwidth = 1;
-		position.anchor = GridBagConstraints.EAST;
-		add(sendMessageButton, position);
-		
-		setVisible(true);
-	}
-	
 	private void subscribeTopic() {
-		java.util.List<String> topics = availableTopicList.getSelectedValuesList();
+		java.util.List<String> topics = gui.getSelectedAvailableTopics();
 		for (String topic : topics) {
-			writeMessage("SUBSCRIBE::" + id + "::" + topic, channelToServer);
-			String answer = readMessage(channelToServer);
+			writeMessage("SUBSCRIBE::" + id + "::" + topic, requestsChannel);
+			String answer = readMessage(requestsChannel);
 			if(answer.equals("OK")) {
 				subscribedTopics.add(topic);
 			}
 			availableTopics.remove(topic);
 		}
-		showTopics();
+		updateTopicsDisplay();
 	}
 	
 	private void unsubscribeTopic() {
-		java.util.List<String> topics = subscribedTopicList.getSelectedValuesList();
+		java.util.List<String> topics = gui.getSelectedSubscribedTopics();
 		for (String topic : topics) {
-			writeMessage("UNSUBSCRIBE::" + id + "::" + topic, channelToServer);
-			String answer = readMessage(channelToServer);
+			writeMessage("UNSUBSCRIBE::" + id + "::" + topic, requestsChannel);
+			String answer = readMessage(requestsChannel);
 			if(answer.equals("OK")) {
 				availableTopics.add(topic);
 			}
 			subscribedTopics.remove(topic);
 		}
-		showTopics();
+		updateTopicsDisplay();
 	}
-	
-	private void showTopics() {
-		DefaultListModel<String> listModel = (DefaultListModel<String>) availableTopicList.getModel();
-		listModel.clear();
-		for (String topic : availableTopics) {
-			listModel.addElement(topic);
-		}
-		listModel = (DefaultListModel<String>) subscribedTopicList.getModel();
-		listModel.clear();
-		for (String topic : subscribedTopics) {
-			listModel.addElement(topic);
-		}	
-	}
-	
+		
 	private void refreshTopics() {
 		String[] topics = sendRequest("TOPICS").split("::");	
-		
 		for (String topic : topics) 
-			availableTopics.add(topic);
-		showTopics();
+			if (!subscribedTopics.contains(topics))
+				availableTopics.add(topic);
+		updateTopicsDisplay();
+	}
+	
+	private void updateTopicsDisplay() {
+		gui.displayAvailableTopics(availableTopics);
+		gui.displaySubscribedTopics(subscribedTopics);
 	}
 
 	public static void main(String[] args) throws UnknownHostException {
 		InetAddress addressIP = InetAddress.getByName("localhost");
-		new Klient(addressIP, 9999);
-				
+		KlientGUI gui = new KlientGUI();
+		Klient klient = new Klient(addressIP, 9999, gui);
+		klient.prepareGUI();
+		klient.start();
 	}
 
 
--- TPO5_WP_S13626/src/zad1/KlientGUI.java
+++ TPO5_WP_S13626/src/zad1/KlientGUI.java
@@ -1,7 +1,178 @@
 package zad1;
 
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import javax.swing.*;
+import javax.swing.text.DefaultCaret;
+
+@SuppressWarnings("serial")
 public class KlientGUI extends JFrame {
-	public KlientGUI {
+	// zmienne dla GUI
+	private JTextArea messageArea;
+	private JList<String> subscribedTopicList;
+	private JList<String> availableTopicList;
+	public JButton subscribeTopicButton;
+	public JButton unsubTopicButton;
+	public JButton closeClientButton;
+	
+	public KlientGUI (){
+		setTitle("Klient");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(600,480);
+
+		setLayout(new GridBagLayout());
+		GridBagConstraints position;
+
+		position = new GridBagConstraints();
+		position.gridx = 0;
+		position.gridy = 0;
+		position.gridwidth = 4;
+		position.anchor = GridBagConstraints.WEST;
+		add(new JLabel("Zarządzanie kanałami:"), position);
+
+		position = new GridBagConstraints();
+		position.gridx = 0;
+		position.gridy = 1;
+		add(Box.createRigidArea(new Dimension(10,5)), position);	
+
+		position = new GridBagConstraints();
+		position.gridx = 0;
+		position.gridy = 2;
+		position.anchor = GridBagConstraints.WEST;
+		add(new JLabel("Kanały dostępne:"), position);
+
+		position = new GridBagConstraints();
+		position.gridx = 4;
+		position.gridy = 2;
+		position.anchor = GridBagConstraints.WEST;
+		add(new JLabel("Kanały subskrybowane:"), position);
+
+		availableTopicList = new JList<String>(new DefaultListModel<String>());
+		availableTopicList.setLayoutOrientation(JList.VERTICAL);
+		JScrollPane availablePane = new JScrollPane(availableTopicList);
+		position = new GridBagConstraints();
+		position.gridx = 0;
+		position.gridy = 3;
+		position.gridheight = 3;
+		position.anchor = GridBagConstraints.WEST;
+		availablePane.setPreferredSize(new Dimension (225,100));
+		add(availablePane,position);
+
+		subscribedTopicList = new JList<String>(new DefaultListModel<String>());
+		subscribedTopicList.setLayoutOrientation(JList.VERTICAL);
+		JScrollPane subscribedPane = new JScrollPane(subscribedTopicList);
+		position = new GridBagConstraints();
+		position.gridx = 4;
+		position.gridy = 3;
+		position.gridheight = 3;
+		position.anchor = GridBagConstraints.EAST;
+		subscribedPane.setPreferredSize(new Dimension (225,100));
+		add(subscribedPane,position);
 		
+		position = new GridBagConstraints();
+		position.gridx = 2;
+		position.gridy = 3;
+		add(Box.createRigidArea(new Dimension(10,15)), position);	
+
+		position = new GridBagConstraints();
+		position.gridx = 1;
+		position.gridy = 4;
+		add(Box.createRigidArea(new Dimension(10,5)), position);	
+
+		JButton subscribeTopicButton = new JButton("Zapisz >>");
+		position = new GridBagConstraints();
+		position.gridx = 2;
+		position.gridy = 4;
+		position.anchor = GridBagConstraints.CENTER;
+		add(subscribeTopicButton, position);
+
+		JButton unsubTopicButton = new JButton("<< Usuń");
+
+		position = new GridBagConstraints();
+		position.gridx = 2;
+		position.gridy = 5;
+		position.anchor = GridBagConstraints.CENTER;
+		add(unsubTopicButton, position);
+
+		position = new GridBagConstraints();
+		position.gridx = 3;
+		position.gridy = 4;
+		add(Box.createRigidArea(new Dimension(10,5)), position);	
+
+		position = new GridBagConstraints();
+		position.gridx = 0;
+		position.gridy = 6;
+		add(Box.createRigidArea(new Dimension(10,20)), position);	
+
+		position = new GridBagConstraints();
+		position.gridx = 0;
+		position.gridy = 7;
+		position.gridwidth = 3;
+		position.anchor = GridBagConstraints.WEST;
+		add(new JLabel("Wiadomość z wybranych kanałów:"), position);
+
+		messageArea = new JTextArea(10,50);
+		messageArea.setLineWrap(true);
+		DefaultCaret caret = (DefaultCaret)messageArea.getCaret();
+		caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
+		JScrollPane messagePane = new JScrollPane(messageArea);
+		messagePane.setViewportView(messageArea);
+		position = new GridBagConstraints();
+		position.gridx = 0;
+		position.gridy = 8;
+		position.gridwidth = 5;
+		add(messagePane, position);
+
+		position = new GridBagConstraints();
+		position.gridx = 0;
+		position.gridy = 9;
+		add(Box.createRigidArea(new Dimension(10,30)), position);	
+
+		JButton closeClientButton = new JButton("Zamknij połączenie");
+		position = new GridBagConstraints();
+		position.gridx = 4;
+		position.gridy = 10;
+		position.gridwidth = 1;
+		position.anchor = GridBagConstraints.EAST;
+		add(closeClientButton, position);	
+	}
+	
+	public void displayMessage(String topic, String message) {
+		String toDisplay = "[ "+LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss"))+" ] kanał: " + topic;
+		messageArea.append(toDisplay + "\r\n");
+		messageArea.append(message + "\r\n");
+		messageArea.append("\r\n");
+	}
+	
+	public void displaySubscribedTopics (List<String> topics) {
+		DefaultListModel<String> listModel = (DefaultListModel<String>) subscribedTopicList.getModel();
+		listModel.clear();
+		for (String topic : topics) {
+			listModel.addElement(topic);
+		}	
+	}
+	
+	public void displayAvailableTopics (List<String> topics) {
+		DefaultListModel<String> listModel = (DefaultListModel<String>) availableTopicList.getModel();
+		listModel.clear();
+		for (String topic : topics) {
+			listModel.addElement(topic);
+		}
+	}
+	
+	public List<String> getSelectedAvailableTopics () {
+		return subscribedTopicList.getSelectedValuesList();
+	}
+	
+	public List<String> getSelectedSubscribedTopics() {
+		return availableTopicList.getSelectedValuesList();
+	}
+	
+	public void start() {
+		setVisible(true);
 	}
 }
